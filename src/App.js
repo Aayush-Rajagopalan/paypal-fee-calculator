786e3d7c89a5f06a429faf535e4c92c2d9487e84
Aayush Rajagopalan <aayush.rajagopalan17@gmail.com>
2022-07-26 08:43:38 +0300

    added tab navigation

--- src/App.js
+++ src/App.js
@@ -1,13 +1,11 @@
 import './App.css';
+import Tabs from './components/tabs';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-      </header>
+      <Tabs/>
     </div>
   );
 }
