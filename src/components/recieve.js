ed5c41d3f44815c3312ffbb0cb305b4e47c9fab3
Aayush Rajagopalan <aayush.rajagopalan17@gmail.com>
2022-07-26 17:50:36 +0300

    finished all the recieving work
    Co-authored-by: Ahmed Baari <ahmed4baari@gmail.com>

--- src/components/recieve.js
+++ src/components/recieve.js
@@ -1,14 +1,34 @@
-import React from "react";
+import React, { useState } from "react";
 import CurrencyInput from "react-currency-input-field";
 
+
+
 function Recieve() {
- const value = 0;
- const result = 0;
+  
+
+  
+  const [value, set_value] = useState(1) 
+  const [result, set_result] = useState(1.52) 
+
+
     function process (amount) {
     const fee = (amount*0.0349)+0.49;
     const result = Number(amount) + Number(fee).toFixed(2) * 1; 
     console.log(result); 
     }
+
+    const updateState = (e) => {
+      if (e==0 || e==null) {
+        set_value(1);
+        set_result(1.52);
+      }
+      else {
+      set_value(e);
+      //set_result(result+1);
+      const final = Number(e) + Number((e*0.0349)+0.49).toFixed(2) * 1;
+      set_result(final);
+      }
+  }
 
 
   return (
@@ -17,22 +37,24 @@ function Recieve() {
       <h2>To Recieve</h2>
       <div className="grid">
         <div className="grid-left">
+        
         <CurrencyInput
                 intlConfig={{ locale: 'en-US', currency: 'USD' }}
                 id="input-example"
                 className="amount"
                 name="input-name"
                 placeholder="Please enter a number"
-                defaultValue={0}
                 step="0.01"
                 decimalsLimit={2}
-                onValueChange={(value, name) => { process(value, name) }}
+                //onChange={updateState}
+                //onValueChange={(value, name) => { process(value, name)}}
+                onValueChange={(e) => {console.log(e); updateState(e)}}
+
         /> 
-          <button className="calculate-button">Calculate</button>
         </div>
         <div className="grid-right">
        <p className="result">
-        To Recieve <span className="blue"> ${ value || 0}</span>, Sender would have to pay <span className="red"> ${result || 0}</span>
+        To Recieve <br/> <span className="blue"> ${value || 1}</span>,<br/> Sender would<br/> have to pay <br/><span className="red"> ${result || 1.52}</span>
         </p>
         </div>
       </div>
